5f513f93f10fa98dcbb184e02e5c3419beba20bd
Nick-Eagles <nick.eagles@libd.org>
2020-02-05 14:47:52 -0500

    Determine QNAME and rg-ID to pass to AriocE by examining FASTQ files. Closes #1.

--- scripts/write_config_encode_reads.R
+++ scripts/write_config_encode_reads.R
@@ -6,10 +6,100 @@ spec <- matrix(c('paired', 'p', 1, 'character', '"single" or "paired"',
                byrow=TRUE, ncol=5)
 opt <- getopt(spec)
 
+print("Writing AriocE configs...")
+
 if (opt$paired == "paired") {
     id = strsplit(system('ls *.fastq', intern=TRUE)[1], "_1.fastq", fixed=TRUE)[[1]]
+    
+    ###################################################################################
+    #  Downstream utilities like Bismark and samblaster are incapable of recognizing
+    #  Illumina Casava-style FASTQ read-IDs (cannot distinguish mate ID from read-ID).
+    #  This code identifies the format style of the FASTQ read ids for a pair of
+    #  mates, and forwards information about QNAME and read group to use for a given
+    #  FASTQ sequence to AriocE.
+    ###################################################################################
+    
+    id1 = system(paste0('cat ', id, '_1.fastq | grep "@" | head -n 1'), intern=TRUE)
+    id2 = system(paste0('cat ', id, '_2.fastq | grep "@" | head -n 1'), intern=TRUE)
+    
+    #----------------------------------------------------------------------------------
+    #  Tries to recognize the read-ID style (checks "old illumina", "illumina casava")
+    #----------------------------------------------------------------------------------
+    
+    print("Examining read ID format in the FASTQ files since data is paired...")
+    recognized = FALSE
+    
+    temp = strsplit(id1, ':')[[1]]
+    cond1 = length(temp) == 5
+    cond2 = all(c('#', '/') %in% strsplit(temp[length(temp)], ''))
+    if (cond1 & cond2) {
+        recognized = TRUE
+        
+        #  Make sure the read IDs for the first site are the same between reads
+        #  (aside from mate ID)
+        first_id = temp[1:4]
+        second_id = strsplit(id2, ':')[[1]][1:4]
+        if (!all(first_id == second_id)) {
+            stop("Read IDs for the first site differ by more than mate ID, between reads")
+        }
+        
+        #  Make sure mate ID differs
+        first_id = temp[5]
+        second_id = strsplit(id2, ':')[[1]][5]
+        if (first_id == second_id) {
+            stop("Despite old Illumina-style read-ID, all fields (including mate ID) were equivalent between reads.")
+        }
+        
+        print("FASTQ read-id is in the old Illumina format, and looks as expected.")
+        
+        qname_field = ''
+        
+        #  Use flowcell id as the "read group"
+        rg_line = '    <rg ID="*:(*):" PL="ILLUMINA" />'
+    }
+    
+    cond1 = length(temp) == 10
+    cond2 = ' ' %in% strsplit(temp[7], '')[[1]]
+    if (cond1 & cond2) {
+        recognized = TRUE
+        
+        #  Make sure the read IDs for the first site are the same between reads
+        #  (aside from mate ID)
+        first_id = temp[c(1:6, 8:10)]
+        second_id = strsplit(id2, ':')[[1]][c(1:6, 8:10)]
+        if (!all(first_id == second_id)) {
+            stop("Read IDs for the first site differ by more than mate ID, between reads")
+        }
+        
+        #  Make sure mate ID differs
+        first_id = temp[7]
+        second_id = strsplit(id2, ':')[[1]][7]
+        if (first_id == second_id) {
+            stop("Despite Illumina casava-style read-ID, all fields (including mate ID) were equivalent between reads.")
+        }
+        
+        print("FASTQ read-id is in the Illumina casava format, and looks as expected.")
+        
+        #  Many utilites fail to recognize read IDs as the same in this format (don't know where mate ID is),
+        #  so we signal here to AriocE to remove the mate ID from the output SAM's QNAME field
+        qname_field = 'QNAME="*:*:*:(*:*:*:*) "'
+        
+        #  Use flowcell id and lane as the "read group"
+        rg_line = '    <rg ID="*:*:(*:*):" PL="ILLUMINA" />'
+    }
+    
+    if (!recognized) {
+        mess = "Warning: could not recognize the FASTQ read-id format. Checked Illumina (old and casava-style)."
+        mess = paste(mess, "Downstream utilities like samblaster and BME might not recognize mate identifier, which")
+        mess = paste(mess, "would halt analysis.")
+        
+        print(mess)
+    }
+    
 } else {
     id = strsplit(system('ls *.fastq', intern=TRUE), ".fastq", fixed=TRUE)[[1]]
+    qname_field = ''
+    rg_line = ''
 }
 
 #  The processed manifest outputted from the "Manifest" process will be in
@@ -25,8 +115,8 @@ idNum = as.integer(idRowNum / 2) + 1
 #  Starting lines
 config_lines = c('<?xml version="1.0" encoding="utf-8"?>', '',
                  '<AriocE>',
-                 '  <dataIn sequenceType="Q" QNAME="*:*:*:(*:*:*:*) ">',
-                 '    <rg ID="*:*:(*:*):" PL="ILLUMINA" />')
+                 paste0('  <dataIn sequenceType="Q" ', qname_field, '>'),
+                 rg_line)
                  
 #  Lines related to the particular reads used for this alignment
 if (opt$paired == "paired") {
@@ -51,3 +141,5 @@ config_lines = c(config_lines,
 #############################################################
 
 writeLines(config_lines, paste0(opt$prefix, '_encode_reads.cfg'))
+
+print("Done writing AriocE configs.")
