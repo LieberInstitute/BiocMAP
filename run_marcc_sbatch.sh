fab12dad4c799dfa292bb94d00d13d1cd8a17e60
Nick-Eagles <nick.eagles@libd.org>
2020-05-07 14:12:18 -0400

    Small adjustments to options in MARCC main script

--- run_marcc_sbatch.sh
+++ run_marcc_sbatch.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-#SBATCH -t 30:00
-#SBATCH --mem=10G
+#SBATCH -t 3:00:00
+#SBATCH --mem=20G
+#SBATCH --ntasks-per-node=6
+#SBATCH -N 1
 
+cd /scratch/groups/ajaffe1/WGBS-Pipeline
 nextflow=/scratch/groups/ajaffe1/rna_sp/RNAsp/Software/nextflow
 module load java
 
 $nextflow run main.nf \
     --sample "paired" \
     --reference "hg38" \
-    -profile marcc
+    -profile marcc \
+    -resume
