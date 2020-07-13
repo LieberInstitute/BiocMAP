12105df6b7430f15aaddf018a29be0aa7bf891fb
Nick-Eagles <nick.eagles@libd.org>
2020-07-13 15:13:39 -0400

    Update hardware resource requirements; default to running the pipeline on test files

--- run_jhpce_qsub.sh
+++ run_jhpce_qsub.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#$ -l bluejay,mem_free=15G,h_vmem=15G,h_fsize=50G
+#$ -l bluejay,mem_free=25G,h_vmem=25G,h_fsize=800G
 #$ -o ./run_jhpce_qsub.log
 #$ -e ./run_jhpce_qsub.log
 #$ -cwd
@@ -10,5 +10,6 @@ export _JAVA_OPTIONS="-Xms8g -Xmx10g"
 nextflow main.nf \
     --sample "paired" \
     --reference "hg38" \
-    -profile jhpce \
-    -resume
+    --input "/dcl01/lieber/ajaffe/Nick/misc/manifests/WGBS_test_small" \
+    --output "/dcl01/lieber/ajaffe/Nick/misc/manifests/WGBS_test_small/out" \
+    -profile jhpce
