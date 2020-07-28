51d19d7c29a15b59233520dd6b118383a667645b
Nick-Eagles <nick.eagles@libd.org>
2020-07-28 15:11:06 -0400

    Fix handling of the last sequence when splitting a reference fasta

--- scripts/split_fasta.sh
+++ scripts/split_fasta.sh
@@ -4,9 +4,14 @@ FASTA=$1
 
 numChrs=$(grep ">c" $FASTA | wc -l)
 lineNums=$(grep -n ">" $FASTA | cut -d : -f 1 | paste -sd " ")
-for CHR in `seq 1 $numChrs`; do
+for CHR in $(seq 1 $numChrs); do
     start=$(echo $lineNums | cut -d " " -f $CHR)
-    end=$(echo $lineNums | cut -d " " -f $(($CHR+1)))
     chr_name=$(sed -n "${start}p;$(($start+1))q" $FASTA | cut -d ">" -f 2 | cut -d " " -f 1)
-    sed -n "$start,$(($end-1))p;$endq" $FASTA > ${chr_name}.fa
+    if [ $CHR == $numChrs ]; then
+        #  Range is from start to EOF
+        sed -n "$start,$ p;$endq" $FASTA > ${chr_name}.fa
+    else
+        end=$(echo $lineNums | cut -d " " -f $(($CHR+1)))
+        sed -n "$start,$(($end-1))p;$endq" $FASTA > ${chr_name}.fa
+    fi
 done
