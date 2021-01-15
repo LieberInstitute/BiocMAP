1d7a5f8571e27aca1e03a6d28f7ff4072c89a38a
Nick-Eagles <nick.eagles@libd.org>
2021-01-15 13:52:00 -0500

    Update how R packages are installed, and which ones are installed

--- scripts/install_R_packages.R
+++ scripts/install_R_packages.R
@@ -1,10 +1,17 @@
 ordinary_packages = c('devtools', 'getopt', 'data.table', 'BiocManager')
-bioc_packages = c('bsseq', 'GenomicRanges', 'HDF5Array')
+bioc_packages = c('bsseq', 'GenomicRanges', 'HDF5Array', 'BiocParallel')
 
-install.packages(ordinary_packages)
+#  Ordinary R packages
+for (p in ordinary_packages) {
+    if (!requireNamespace(p, quietly=TRUE)) {
+        install.packages(p)
+    }
+}
 
+#  Bioconductor packages
 library('BiocManager')
 BiocManager::install(bioc_packages)
 
+#  GitHub packages
 library('devtools')
 install_github('LieberInstitute/jaffelab')
