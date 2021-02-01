c0eb04fc92450776afdeda9833f4af2a16735df5
Nick-Eagles <nick.eagles@libd.org>
2021-02-01 15:48:08 -0500

    Add a 'main' script for the second half at JHPCE; correctly ignore output logs in git

---
+++ run_second_half_jhpce.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#$ -l bluejay,mem_free=25G,h_vmem=25G,h_fsize=800G
+#$ -o ./run_second_half_jhpce.log
+#$ -e ./run_second_half_jhpce.log
+#$ -cwd
+
+module load nextflow
+export _JAVA_OPTIONS="-Xms8g -Xmx10g"
+
+nextflow second_half.nf \
+    --sample "paired" \
+    --reference "hg38" \
+    --input "/dcl01/lieber/ajaffe/Nick/misc/manifests/WGBS_test_master" \
+    --output "/dcl01/lieber/ajaffe/Nick/misc/manifests/WGBS_test_master/out" \
+    -profile second_half_jhpce
