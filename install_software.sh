edf54d8d371ac862e7456eb7f3226693985a00ff
Nick-Eagles <nick.eagles@libd.org>
2021-04-15 11:46:01 -0400

    Ensure installation script exits upon any errors

--- install_software.sh
+++ install_software.sh
@@ -21,69 +21,69 @@ if [ -x "$(command -v java)" ]; then
     
     #  Bismark (0.23.0)
     
-    wget https://github.com/FelixKrueger/Bismark/archive/0.23.0.tar.gz && \
-        tar -xzf 0.23.0.tar.gz
-        cp Bismark-0.23.0/bismark* bin/
-        cp Bismark-0.23.0/coverage2cytosine bin/
+    wget https://github.com/FelixKrueger/Bismark/archive/0.23.0.tar.gz
+    tar -xzf 0.23.0.tar.gz
+    cp Bismark-0.23.0/bismark* bin/
+    cp Bismark-0.23.0/coverage2cytosine bin/
     
     #  fastqc (0.11.8)  -------------------------------------------------------
     
-    wget https://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.11.8.zip && \
-        unzip fastqc_v0.11.8.zip && \
-        chmod -R 775 FastQC
-        cp FastQC/fastqc bin/
+    wget https://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.11.8.zip
+    unzip fastqc_v0.11.8.zip
+    chmod -R 775 FastQC
+    cp FastQC/fastqc bin/
         
     #  MethylDackel (latest)
     
     ##  Install libBigWig, a dependency
-    git clone git@github.com:dpryan79/libBigWig.git && \
-            cd libBigWig && \
-            make prefix=$INSTALL_DIR install
-            cd $INSTALL_DIR
+    git clone git@github.com:dpryan79/libBigWig.git
+    cd libBigWig
+    make prefix=$INSTALL_DIR install
+    cd $INSTALL_DIR
       
     ##  Install htslib (1.10.2), a dependency     
-    wget https://github.com/samtools/htslib/releases/download/1.10.2/htslib-1.10.2.tar.bz2 -O htslib.tar.bz2 && \
-            tar -xjf htslib.tar.bz2 && \
-            cd htslib-1.10.2 && \
-            ./configure prefix=$INSTALL_DIR && \
-            make && \
-            make install
-            cd $INSTALL_DIR
-            mv htslib-1.10.2 htslib
+    wget https://github.com/samtools/htslib/releases/download/1.10.2/htslib-1.10.2.tar.bz2 -O htslib.tar.bz2
+    tar -xjf htslib.tar.bz2
+    cd htslib-1.10.2
+    ./configure prefix=$INSTALL_DIR
+    make
+    make install
+    cd $INSTALL_DIR
+    mv htslib-1.10.2 htslib
     
     ##  MethylDackel itself (latest)
-    git clone https://github.com/dpryan79/MethylDackel.git && \
-        cd MethylDackel && \
-        make install CFLAGS="-O3 -Wall -I$INSTALL_DIR/include " LIBS="-L$INSTALL_DIR/lib" prefix=$INSTALL_DIR/bin LIBBIGWIG="$INSTALL_DIR/libBigWig/libBigWig.a"
-        cd $INSTALL_DIR
+    git clone https://github.com/dpryan79/MethylDackel.git
+    cd MethylDackel
+    make install CFLAGS="-O3 -Wall -I$INSTALL_DIR/include " LIBS="-L$INSTALL_DIR/lib" prefix=$INSTALL_DIR/bin LIBBIGWIG="$INSTALL_DIR/libBigWig/libBigWig.a"
+    cd $INSTALL_DIR
       
     #  Install packages that will be used by the pipeline
     Rscript ../scripts/install_R_packages.R
     
     #  samblaster (v.0.1.26)
     
-    wget https://github.com/GregoryFaust/samblaster/releases/download/v.0.1.26/samblaster-v.0.1.26.tar.gz && \
-        tar -xzf samblaster-v.0.1.26.tar.gz && \
-        cd samblaster-v.0.1.26 && \
-        make
-        cp samblaster ../bin/
-        cd $INSTALL_DIR
+    wget https://github.com/GregoryFaust/samblaster/releases/download/v.0.1.26/samblaster-v.0.1.26.tar.gz
+    tar -xzf samblaster-v.0.1.26.tar.gz
+    cd samblaster-v.0.1.26
+    make
+    cp samblaster ../bin/
+    cd $INSTALL_DIR
     
     #  samtools (1.10)  -------------------------------------------------------
     
-    wget https://github.com/samtools/samtools/releases/download/1.10/samtools-1.10.tar.bz2 -O samtools.tar.bz2 && \
-        tar -xjf samtools.tar.bz2 && \
-        cd samtools-1.10 && \
-        ./configure prefix=$INSTALL_DIR && \
-        make && \
-        make install
-        cd $INSTALL_DIR
+    wget https://github.com/samtools/samtools/releases/download/1.10/samtools-1.10.tar.bz2 -O samtools.tar.bz2
+    tar -xjf samtools.tar.bz2
+    cd samtools-1.10
+    ./configure prefix=$INSTALL_DIR
+    make
+    make install
+    cd $INSTALL_DIR
         
     #  Trim Galore!
     
-    wget https://github.com/FelixKrueger/TrimGalore/archive/0.6.6.tar.gz && \
-        tar -xzf 0.6.6.tar.gz
-        cp TrimGalore-0.6.6/trim_galore bin/
+    wget https://github.com/FelixKrueger/TrimGalore/archive/0.6.6.tar.gz
+    tar -xzf 0.6.6.tar.gz
+    cp TrimGalore-0.6.6/trim_galore bin/
         
     #  Clean up compressed files
     rm $INSTALL_DIR/*.tar.gz
