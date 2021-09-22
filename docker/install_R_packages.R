4ba8e942a53fd39e798eb4117a95cfe9e94d4cf4
Nick-Eagles <nick.eagles@libd.org>
2021-09-22 14:46:56 -0400

    Include additional R packages in the 'bioc_kallisto' docker image to enable compatability with SPEAQeasy

--- docker/install_R_packages.R
+++ docker/install_R_packages.R
@@ -1,10 +1,21 @@
 #  While the main version of this script is in the 'scripts' directory, a
 #  specific copy used while producing the 'bioc_kallisto:3.11' docker image is
-#  kept here for reproducibility, in case changes are made to the original
-#  script.
+#  kept here for reproducibility. In addition, packages not strictly required
+#  by this pipeline are installed into the image so that the image can also be
+#  used by SPEAQeasy (https://github.com/LieberInstitute/SPEAQeasy).
 
-ordinary_packages = c('remotes', 'getopt', 'data.table', 'BiocManager', 'here')
-bioc_packages = c('bsseq', 'GenomicRanges', 'HDF5Array', 'BiocParallel')
+ordinary_packages = c(
+    'remotes', 'getopt', 'data.table', 'BiocManager', 'here', 'devtools',
+    'matrixStats', 'plyr', 'rafalib', 'RColorBrewer', 'sessioninfo', 'styler'
+)
+
+bioc_packages = c(
+    'bsseq', 'GenomicRanges', 'HDF5Array', 'BiocParallel', 'biomaRt',
+    'Biostrings', 'BSgenome.Hsapiens.UCSC.hg19', 'BSgenome.Hsapiens.UCSC.hg38',
+    'BSgenome.Mmusculus.UCSC.mm10', 'BSgenome.Rnorvegicus.UCSC.rn6',
+    'DelayedArray', 'derfinder', 'GenomicFeatures', 'org.Hs.eg.db',
+    'org.Mm.eg.db', 'org.Rn.eg.db', 'rtracklayer', 'SummarizedExperiment'
+)
 
 #  Ordinary R packages
 for (p in ordinary_packages) {
