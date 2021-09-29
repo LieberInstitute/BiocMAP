cf8453bc3c1121d968d7e4ef61ef86003a97fa72
Nick-Eagles <nick.eagles@libd.org>
2021-09-29 13:58:30 -0400

    Rewrite grid-test script to be more general (thus simpler to edit when needed), possibly at the expense of code readability

--- scripts/grid_test_jhpce.R
+++ scripts/grid_test_jhpce.R
@@ -11,14 +11,21 @@ library('getopt')
 #  "--with_lambda"). This assumption is not made for 'major' options like
 #  "--sample".
 
-#  "Major" options
+#  "Major" options (apply to both first and second halves)
 sample_opts = c('single', 'paired')
 ref_opts = c('hg38', 'hg19')
 
-#  "Minor" options
-trim_opts = c('skip', 'adaptive', 'force')
-bme_opts = c('true', 'false')
-lambda_opts = c('true', 'false')
+#  "Minor" options (for first and second halves, respectively)
+minor_opts_first = list(
+    c('trim_mode'), 
+    list(c('skip', 'adaptive', 'force'))
+)
+
+minor_opts_second = list(
+    c('use_bme', 'with_lambda'),
+    list(c('true', 'false'), c('true', 'false'))
+)
+
 
 spec = matrix(
     c("base_dir", "d", 1, "character", "full path to directory to place grid-test files"),
@@ -27,6 +34,23 @@ spec = matrix(
 opt <- getopt(spec)
 
 dir.create(opt$base_dir, showWarnings=FALSE)
+
+#  Shuffle minor options randomly
+minor_opts_first = list(
+    minor_opts_first[[1]],
+    lapply(
+        minor_opts_first[[2]],
+        function(x) sample(x, size=length(x), replace=FALSE)
+    )
+)
+
+minor_opts_second = list(
+    minor_opts_second[[1]],
+    lapply(
+        minor_opts_second[[2]],
+        function(x) sample(x, size=length(x), replace=FALSE)
+    )
+)
 
 
 first_half_base_command = c(
@@ -51,67 +75,61 @@ second_half_base_command = c(
     '    -profile second_half_jhpce \\'
 )
 
-###############################################################################
-
-#  Shuffle minor options randomly
-trim_opts = sample(trim_opts, size=length(trim_opts), replace=FALSE)
-bme_opts = sample(bme_opts, size=length(bme_opts), replace=FALSE)
-lambda_opts = sample(lambda_opts, size=length(lambda_opts), replace=FALSE)
 
 index = 1
 for (sample_opt in sample_opts) {
     for (ref_opt in ref_opts) {
-        #  Determine value of minor options
-        trim_opt = trim_opts[(index %% length(trim_opts)) + 1]
-        bme_opt = bme_opts[(index %% length(bme_opts)) + 1]
-        lambda_opt = lambda_opts[(index %% length(lambda_opts)) + 1]
+        for (half in c('first', 'second')) {
+            #  Use the appropriate variables for this half
+            if (half == 'first') {
+                base_command = first_half_base_command
+                minor_opts = minor_opts_first
+            } else {
+                base_command = second_half_base_command
+                minor_opts = minor_opts_second
+            }
+            
+            #  Determine command
+            log_path = file.path(
+                opt$base_dir,
+                paste0('grid_test_', half, '_', index, '.log')
+            )
         
-        #  Determine command for first half
-        log_path = file.path(
-            opt$base_dir,
-            paste0('grid_test_first_', index, '.log'
-        )
+            command = c(
+                paste('#$ -o', log_path),
+                paste('#$ -e', log_path),
+                base_command,
+                paste0('    --sample "', sample_opt, '" \\'),
+                paste0('    --reference "', ref_opt, '" \\'),
+                paste0('    --annotation "', here('ref'), '" \\'),
+                paste0('    -w "', opt$base_dir, '/work_', half, '_', index, '" \\'),
+                paste0('    --output "', opt$base_dir, '/out_', half, '_', index, '" \\')
+            )
         
-        first_half_command = c(
-            paste('#$ -o', log_path),
-            paste('#$ -e', log_path),
-            first_half_base_command,
-            paste0('    --trim_mode "', trim_opt, '" \\'),
-            paste0('    --sample "', sample_opt, '" \\'),
-            paste0('    --reference "', ref_opt, '" \\'),
-            paste0('    --annotation "', here('ref'), '" \\'),
-            paste0('    -w "', opt$base_dir, '/work_first_', index, '" \\'),
-            paste0('    --output "', opt$base_dir, '/out_first_', index, '"')
-        )
+            #  Add command options corresponding to the "minor_opts"
+            for (i in 1:length(minor_opts[[1]])) {
+                #  The possible values this command option takes as an argument,
+                #  followed by the particular value to use for this "element" of
+                #  the grid test
+                opt_values = minor_opts[[2]][[i]]
+                this_value = opt_values[(index %% length(opt_values)) + 1]
+                
+                command_opt = paste0(
+                    '    --', minor_opts[[1]][i], ' ', this_value
+                )
+                
+                if (i < length(minor_opts[[1]])) {
+                    command_opt = paste0(command_opt, ' \\')
+                }
+                
+                command = c(command, command_opt)
+            }
         
-        #  Write into shell script and submit as a job
-        shell_path = file.path(opt$base_dir, paste0('run_first_', index, '.sh'))
-        writeLines(first_half_command, con=shell_path)
-        #system(paste('qsub', shell_path))
-        
-        #  Determine command for second half
-        log_path = file.path(
-            opt$base_dir,
-            paste0('grid_test_second_', index, '.log'
-        )
-        
-        second_half_command = c(
-            paste('#$ -o', log_path),
-            paste('#$ -e', log_path),
-            second_half_base_command,
-            paste0('    --with_lambda=', lambda_opt, ' \\'),
-            paste0('    --use_bme=', bme_opt, ' \\'), 
-            paste0('    --sample "', sample_opt, '" \\'),
-            paste0('    --reference "', ref_opt, '" \\'),
-            paste0('    --annotation "', here('ref'), '" \\'),
-            paste0('    -w "', opt$base_dir, '/work_second_', index, '" \\'),
-            paste0('    --output "', opt$base_dir, '/out_second_', index, '"')
-        )
-        
-        #  Write into shell script and submit as a job
-        shell_path = file.path(opt$base_dir, paste0('run_second_', index, '.sh'))
-        writeLines(second_half_command, con=shell_path)
-        #system(paste('qsub', shell_path))
+            #  Write into shell script and submit as a job
+            shell_path = file.path(opt$base_dir, paste0('run_', half, '_', index, '.sh'))
+            writeLines(command, con=shell_path)
+            #system(paste('qsub', shell_path))
+        }
         
         index = index + 1
     }
