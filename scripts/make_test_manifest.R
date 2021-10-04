9a2004d71c8785f049d82a43bc16b4a5130b716a
Nick-Eagles <nick.eagles@libd.org>
2021-10-04 15:03:46 -0400

    Remove a deprecated script

--- scripts/make_test_manifest.R
+++
@@ -1,8 +0,0 @@
-library('jaffelab')
-
-man_dir = paste0(getwd(), '/test')
-R1 = paste0(man_dir, '/', list.files(man_dir, pattern = '*_R1\\.fastq'))
-R2 = paste0(man_dir, '/', list.files(man_dir, pattern = '*_R2\\.fastq'))
-man_text = paste(R1, 0, R2, 0, basename(ss(R1, '_R1\\.fastq')), sep = "\t")
-
-writeLines(man_text, paste0(man_dir, '/samples.manifest'))
