de916940f477589cf071d7f30d95c062ed942ab9
Nick-Eagles <nick.eagles@libd.org>
2022-02-11 16:28:36 -0500

    Write a basic script to style the RMD and render to PDF

---
+++ documentation/example_analysis/render.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#$ -cwd
+#$ -o render.log
+#$ -e render.log
+#$ -l bluejay,mf=50G,h_vmem=50G
+
+RMD_FILE=example_analysis.Rmd
+
+module load conda_R/4.1.x
+module load tex/2017
+
+#  Style code
+Rscript -e "styler::style_file('$RMD_FILE', transformers = biocthis::bioc_style()"
+
+#  Render into PDF (as defined in the YAML header)
+Rscript -e "rmarkdown::render('$RMD_FILE')"
