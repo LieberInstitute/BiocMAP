448c9785582f0fbb314663c92ca14f71f5e016c6
Nick-Eagles <nick.eagles@libd.org>
2022-02-16 11:31:00 -0500

    Fix a syntax issue

--- documentation/example_analysis/render.sh
+++ documentation/example_analysis/render.sh
@@ -2,7 +2,7 @@
 #$ -cwd
 #$ -o render.log
 #$ -e render.log
-#$ -l bluejay,mf=50G,h_vmem=50G
+#$ -l mf=50G,h_vmem=50G
 
 RMD_FILE=example_analysis.Rmd
 
@@ -10,7 +10,7 @@ module load conda_R/4.1.x
 module load tex/2017
 
 #  Style code
-Rscript -e "styler::style_file('$RMD_FILE', transformers = biocthis::bioc_style()"
+Rscript -e "styler::style_file('$RMD_FILE', transformers = biocthis::bioc_style())"
 
 #  Render into PDF (as defined in the YAML header)
 Rscript -e "rmarkdown::render('$RMD_FILE')"
