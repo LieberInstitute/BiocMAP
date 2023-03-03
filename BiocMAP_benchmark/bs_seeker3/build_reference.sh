21c54e40fabedf3f6d0861b6634a2f2f4585902c
Nick-Eagles <nick.eagles@libd.org>
2023-03-03 11:59:24 -0500

    Add a script to build the reference genome for BS-Seeker3

---
+++ BiocMAP_benchmark/bs_seeker3/build_reference.sh
@@ -0,0 +1,22 @@
+#$ -cwd
+#$ -o logs/build_reference.log
+#$ -e logs/build_reference.log
+#$ -l mem_free=20G,h_vmem=20G,h_fsize=100G
+
+echo "**** Job starts ****"
+date
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SGE_TASK_ID}"
+
+FASTA=$MYSCRATCH/BiocMAP_ref_temp/hg38_gencode_v34_main/assembly_hg38_gencode_v34_main.fa
+
+conda activate $PWD/bs_env
+cd bs3
+./bs3-build -f $FASTA -L 5
+
+echo "**** Job ends ****"
+date
