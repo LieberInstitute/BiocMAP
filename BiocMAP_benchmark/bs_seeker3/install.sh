b41b971fbfbc9804f82fbe4b372d82fde32315ad
Nick-Eagles <nick.eagles@libd.org>
2023-03-03 15:37:30 -0500

    A newer version of GCC appears to be needed to properly install python dependencies

--- BiocMAP_benchmark/bs_seeker3/install.sh
+++ BiocMAP_benchmark/bs_seeker3/install.sh
@@ -2,6 +2,8 @@
 conda create -y -n mamba_env -c conda-forge mamba
 conda activate mamba_env
 
+module load gcc/5.5.0
+
 #   Install python dependencies in new conda environment
 mamba create -y -p $PWD/bs_env python=2.7
 conda activate $PWD/bs_env
