73c6457616858a0b3d0a930cb6c4164180816622
Nick-Eagles <nick.eagles@libd.org>
2023-03-13 16:58:03 -0400

    Move annotation files from fastscratch to dcs04

--- BiocMAP_benchmark/bs_seeker3/build_reference.sh
+++ BiocMAP_benchmark/bs_seeker3/build_reference.sh
@@ -2,7 +2,7 @@
 #$ -N build_reference
 #$ -o logs/build_reference.log
 #$ -e logs/build_reference.log
-#$ -l mem_free=80G,h_vmem=80G,h_fsize=100G
+#$ -l mem_free=200G,h_vmem=200G,h_fsize=100G
 
 echo "**** Job starts ****"
 date
@@ -12,7 +12,7 @@ echo "Job id: ${JOB_ID}"
 echo "Job name: ${JOB_NAME}"
 echo "Hostname: ${HOSTNAME}"
 
-FASTA=$MYSCRATCH/BiocMAP_ref_temp/hg38_gencode_v34_main/assembly_hg38_gencode_v34_main.fa
+FASTA=/dcs04/lieber/lcolladotor/BiocMAP_benchmark_LIBD001/BiocMAP_ref_temp/hg38_gencode_v34_main/assembly_hg38_gencode_v34_main.fa
 
 conda activate $PWD/bs_env
 cd bs3
