79ca649e3a6a6a93c4849623cff96921f4589677
Nick-Eagles <nick.eagles@libd.org>
2023-03-13 14:23:29 -0400

    Make changes meant to be committed last commit

--- BiocMAP_benchmark/BiocMAP_dir/run_second_half_jhpce.sh
+++ BiocMAP_benchmark/BiocMAP_dir/run_second_half_jhpce.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
-#$ -l bluejay,mem_free=25G,h_vmem=25G,h_fsize=800G
-#$ -o ./run_second_half_jhpce.log
-#$ -e ./run_second_half_jhpce.log
+#$ -l mem_free=25G,h_vmem=25G,h_fsize=800G
+#$ -o logs/run_second_half_jhpce.log
+#$ -e logs/run_second_half_jhpce.log
 #$ -cwd
 
-#  After running 'install_software.sh', this should point to the directory
-#  where BiocMAP was installed, and not say "$PWD"
-ORIG_DIR=/users/neagles/LIBD_WGBS_testing/BiocMAP_benchmark/BiocMAP_dir/BiocMAP
+REPO_DIR=$(git rev-parse --show-toplevel)
+ANN_DIR=/dcs04/lieber/lcolladotor/BiocMAP_benchmark_LIBD001/BiocMAP_ref_temp
+BASE_WORK_DIR=/dcs04/lieber/lcolladotor/BiocMAP_benchmark_LIBD001/BiocMAP
+
+mkdir -p $BASE_WORK_DIR
 
 module load nextflow/20.01.0
 export _JAVA_OPTIONS="-Xms8g -Xmx10g"
 
-nextflow $ORIG_DIR/second_half.nf \
-    --annotation "/fastscratch/myscratch/neagles/BiocMAP_ref_temp" \
+nextflow $REPO_DIR/BiocMAP_benchmark/BiocMAP_dir/BiocMAP/second_half.nf \
+    --annotation "$ANN_DIR" \
     --sample "paired" \
     --reference "hg38" \
+    --input "$REPO_DIR/BiocMAP_benchmark" \
+    -w "$BASE_WORK_DIR/work" \
+    --output "$BASE_WORK_DIR/out" \
+    -with-report "logs/second_half_exec_report.html" \
     -profile second_half_jhpce
-
-#   Log successful runs on non-test data in a central location. Please adjust
-#   the log path here if it is changed at the top!
-bash $ORIG_DIR/scripts/track_runs.sh $PWD/run_second_half_jhpce.log second
