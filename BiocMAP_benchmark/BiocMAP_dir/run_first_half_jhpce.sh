1d5718575e326fe91497770797eb48d3c62edb67
Nick-Eagles <nick.eagles@libd.org>
2023-04-07 16:25:37 -0400

    Use default trimming settings; rerun

--- BiocMAP_benchmark/BiocMAP_dir/run_first_half_jhpce.sh
+++ BiocMAP_benchmark/BiocMAP_dir/run_first_half_jhpce.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #$ -l mem_free=25G,h_vmem=25G,h_fsize=800G
-#$ -o logs/run_first_half_jhpce.log
-#$ -e logs/run_first_half_jhpce.log
+#$ -o logs/run_first_half_jhpce_2.log
+#$ -e logs/run_first_half_jhpce_2.log
 #$ -cwd
 
 REPO_DIR=$(git rev-parse --show-toplevel)
 ANN_DIR=/dcs04/lieber/lcolladotor/BiocMAP_benchmark_LIBD001/BiocMAP_ref_temp
-BASE_WORK_DIR=/dcs04/lieber/lcolladotor/BiocMAP_benchmark_LIBD001/BiocMAP
+BASE_WORK_DIR=/dcs04/lieber/lcolladotor/BiocMAP_benchmark_LIBD001/BiocMAP_2
 
 mkdir -p $BASE_WORK_DIR
 
@@ -17,9 +17,8 @@ nextflow $REPO_DIR/BiocMAP_benchmark/BiocMAP_dir/BiocMAP/first_half.nf \
     --annotation "$ANN_DIR" \
     --sample "paired" \
     --reference "hg38" \
-    --trim_mode "force" \
     --input "$REPO_DIR/BiocMAP_benchmark" \
     -w "$BASE_WORK_DIR/work" \
     --output "$BASE_WORK_DIR/out" \
-    -with-report "logs/first_half_exec_report.html" \
+    -with-report "logs/first_half_exec_report_2.html" \
     -profile first_half_jhpce
