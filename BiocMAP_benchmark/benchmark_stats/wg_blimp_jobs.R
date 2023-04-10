fcd7fd612c7dc25bb29d40a2ff03d4218cfed12f
Nick-Eagles <nick.eagles@libd.org>
2023-04-10 16:21:28 -0400

    Extract more information from SGE logs

--- BiocMAP_benchmark/benchmark_stats/wg_blimp_jobs.R
+++ BiocMAP_benchmark/benchmark_stats/wg_blimp_jobs.R
@@ -1,9 +1,13 @@
 library('here')
-library('tidyverse')
 
 source(here('BiocMAP_benchmark', 'benchmark_stats', 'sge_job_info.R'))
 
 log_path = here('BiocMAP_benchmark', 'wg_blimp', 'logs', 'run.log')
+var_names = c(
+    'maxvmem', 'ru_maxrss', 'exit_status', 'cpu', 'start_time', 'end_time',
+    'slots'
+)
+
 log_text = readLines(log_path)
 
 job_info = log_text[grep('^Submitted job .* with external jobid', log_text)] |>
@@ -17,3 +21,33 @@ job_df = tibble(
         str_extract('job [0-9]+ \\(') |>
         str_remove_all('[ job\\(]')
 )
+
+a = lapply(
+    job_df$job_id,
+    function (job_id) get_job_info(
+        job_id, month_limit = 3, fail_on_error = FALSE
+    )
+)
+
+#   Extract variables of interest from the 'qacct' output
+for (var_name in var_names) {
+    job_df[[var_name]] = sapply(
+        a,
+        function(x) {
+            if (any(is.na(x))) {
+                return(NA)
+            } else {
+                return(
+                    x[grep(paste0('^', var_name), x)] |>
+                        str_remove_all(paste0('(', var_name, ' *| *$)'))
+                )
+            }
+        }
+    )
+}
+
+#   Take only jobs that finished successfully; extract start and end times in
+#   'chron' format
+job_df = job_df |>
+    filter((!is.na(exit_status)) & (exit_status == "0")) |>
+    mutate(start_time = to_chron(start_time), end_time = to_chron(end_time))
