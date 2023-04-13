593d5fbc67a02e0d66f39c7c7d19dae159a31230
Nick-Eagles <nick.eagles@libd.org>
2023-04-13 16:56:43 -0400

    Begin some experimental code to track total concurrent CPUs used at different bins of time

--- BiocMAP_benchmark/benchmark_stats/wg_blimp_jobs.R
+++ BiocMAP_benchmark/benchmark_stats/wg_blimp_jobs.R
@@ -50,4 +50,29 @@ for (var_name in var_names) {
 #   'chron' format
 job_df = job_df |>
     filter((!is.na(exit_status)) & (exit_status == "0")) |>
-    mutate(start_time = to_chron(start_time), end_time = to_chron(end_time))
+    mutate(
+        start_time = to_chron(start_time),
+        end_time = to_chron(end_time),
+        slots = as.numeric(slots)
+    )
+
+#   Compute max number of concurrent CPUs used at any time point
+bins = 10000
+bin_size_time = (max(job_df$end_time) - min(job_df$start_time)) / bins
+bin_size = as.numeric(bin_size_time)
+
+print(paste('Using bin size of', bin_size_time))
+
+total_cpus = rep.int(0, bins)
+for (i in 1:bins) {
+    bin_start = min(job_df$start_time) + (i - 1) * bin_size
+    bin_end = bin_start + bin_size
+    
+    total_cpus[i] = job_df |>
+        #   A given job is counted only if it it running throughout the full bin
+        filter(start_time <= bin_start, end_time > bin_end) |>
+        summarize(total_cpus = sum(slots)) |>
+        pull(total_cpus)
+}
+
+ggplot(job_df, aes(x = start_time)) + geom_histogram(aes(weight = slots))
