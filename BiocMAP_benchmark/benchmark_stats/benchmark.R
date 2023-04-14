c00116d87c04948a2b4f311dad2292aa3865b5a0
Nick-Eagles <nick.eagles@libd.org>
2023-04-14 16:21:55 -0400

    Compute several pipeline performance metrics; implement an algorithm to determine total wallclock time, accounting for failed processes that must be restarted, and other gaps where the pipeline is not running

--- BiocMAP_benchmark/benchmark_stats/benchmark.R
+++ BiocMAP_benchmark/benchmark_stats/benchmark.R
@@ -1,6 +1,107 @@
 #   Read in table of SGE job info for all tested pipelines and perform stats/
 #   visualizations
 
+
+job_df_path = here(
+    'BiocMAP_benchmark', 'benchmark_stats', 'combined_job_info.csv'
+)
+
+job_df = read.csv(job_df_path) |>
+    as_tibble() |>
+    mutate(
+        start_time = strptime(start_time, format = "%Y-%m-%d %H:%M:%S"),
+        end_time = strptime(end_time, format = "%Y-%m-%d %H:%M:%S"),
+        qsub_time = strptime(qsub_time, format = "%Y-%m-%d %H:%M:%S")
+    )
+
+################################################################################
+#   Adjust times for BiocMAP: stitch together first and second halves, since we
+#   aren't counting the in-between time when BiocMAP isn't running
+################################################################################
+
+#   Find end of first half and start of second half
+
+first_end = job_df |>
+    filter(
+        software == "BiocMAP", grepl('FilterAlignments|MakeRules', jobname)
+    ) |>
+    summarize(pipeline_end = max(end_time)) |>
+    pull(pipeline_end)
+
+second_start = job_df |>
+    filter(software == "BiocMAP", grepl('PreprocessInputs', jobname)) |>
+    summarize(pipeline_start = max(start_time)) |>
+    pull(pipeline_start)
+
+#   Move the BiocMAP second-half processes back so the first one starts when the
+#   latest first-half process ends. Effectively, this makes the two BiocMAP
+#   halves a single seamless workflow
+job_df = job_df |>
+    mutate(
+        start_time = case_when(
+            (software == "BiocMAP") & (start_time >= second_start) ~ 
+                start_time - (second_start - first_end),
+            TRUE ~ start_time
+        ),
+        end_time = case_when(
+            (software == "BiocMAP") & (end_time > second_start) ~ 
+                end_time - (second_start - first_end),
+            TRUE ~ end_time
+        )
+    )
+
+################################################################################
+#   Stats and visualizations
+################################################################################
+
+#   Based on https://stackoverflow.com/a/28938694. First, merge overlapping
+#   jobs to form the minimal set of disjoint time periods, allowing for 3-minute
+#   gaps (Nextflow/Snakemake has a small overhead of processing between
+#   submitted jobs). Here, 'qsub_time' is used, since in extreme cases, a job
+#   will take a couple hours before starting once submitted.
+#
+#   Next, sum up the wallclock time for these disjoint intervals to determine
+#   the total wallclock time for each pipeline software.
+print('Total wallclock times:')
+wallclock_df = job_df |>
+    mutate(end_time = end_time + 180) |>
+    arrange(software, qsub_time, end_time) |>
+    group_by(software) |>
+    mutate(
+        index = c(
+            0,
+            cumsum(
+                as.numeric(lead(qsub_time)) > cummax(as.numeric(end_time))
+            )[-n()]
+        )
+    ) |>
+    group_by(software, index) |>
+    summarize(
+        start_time = first(start_time), end_time = last(end_time) - 180
+    ) |>
+    #   Now add up disjoint intervals to compute total wallclock time for each
+    #   pipeline software
+    group_by(software) |>
+    summarize(
+        wallclock_days = as.numeric(
+            sum(end_time - start_time), units = "days"
+        )
+    ) |>
+    ungroup()
+
+#   Compute other statistics
+stats_df = job_df |>
+    group_by(software) |>
+    summarize(
+        total_mem_TB_hours = sum(mem) / 1e12 / 3600,
+        vmem_frac_used = sum(maxvmem) / sum(requested_h_vmem),
+        cpu_hours = sum(as.numeric(cpu |> str_remove('s'))) / 3600
+    ) |>
+    left_join(wallclock_df)
+
+print('Stats:')
+print(stats_df)
+
 #   Compute max number of concurrent CPUs used at any time point
 bins = 10000
 bin_size_time = (max(job_df$end_time) - min(job_df$start_time)) / bins
